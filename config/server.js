6ecc881e8c480612d22df2e32b9dc899bc4f9a4b
benyamynbrkyc <benjamin.brkic.a@gmail.com>
2021-10-15 00:39:18 +0200

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '96fae9f0070ad3de2498cc6e146c119a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b46741a6-9a5f-4bcd-b545-9e0b0b69edf5'
+};
