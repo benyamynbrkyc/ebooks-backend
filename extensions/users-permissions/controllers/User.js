a54251d6cc337f563a13bb50e8c2126ace861a8d
benyamynbrkyc <benjamin.brkic.a@gmail.com>
2021-10-20 21:16:32 +0200

    add routes.json for updateMe

---
+++ extensions/users-permissions/controllers/User.js
@@ -0,0 +1,11 @@
+const _ = require("lodash");
+const { sanitizeEntity } = require("strapi-utils");
+
+const sanitizeUser = (user) =>
+  sanitizeEntity(user, {
+    model: strapi.query("user", "users-permissions").model,
+  });
+
+const formatError = (error) => [
+  { messages: [{ id: error.id, message: error.message, field: error.field }] },
+];
