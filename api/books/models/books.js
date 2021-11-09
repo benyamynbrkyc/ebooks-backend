3cf668fb0b29c5afb0aaf04dfeb45f5c902d3b72
benyamynbrkyc <benjamin.brkic.a@gmail.com>
2021-11-09 13:28:12 +0100

    create book afterCreate handler

--- api/books/models/books.js
+++ api/books/models/books.js
@@ -1,10 +1,33 @@
 "use strict";
 
+const ebookConverter = require("node-ebook-converter");
+
+// TODO: figure out what to do with this
+
 module.exports = {
   lifecycles: {
     async afterCreate(result, data) {
       console.log("**** created book");
       console.log(result);
+
+      return;
+      const eBookPdfUrl = process.cwd() + "/public" + result.e_book_pdf.PDF.url;
+
+      try {
+        const response = await ebookConverter.convert({
+          input: eBookPdfUrl,
+          output:
+            process.cwd() +
+            "/public/uploads/ " +
+            result.e_book_pdf.PDF.name +
+            ".epub",
+          authors: result.author,
+        });
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+        throw new Error("Could not convert");
+      }
     },
   },
 };
