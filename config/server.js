8ef1a1d624f6b4cdfb06e180cffee253bb625e2f
benyamynbrkyc <benjamin.brkic.a@gmail.com>
2021-12-29 21:47:31 +0100

    set up url for reset password

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("URL", "localhost"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '96fae9f0070ad3de2498cc6e146c119a'),
+      secret: env("ADMIN_JWT_SECRET", "96fae9f0070ad3de2498cc6e146c119a"),
     },
   },
 });
