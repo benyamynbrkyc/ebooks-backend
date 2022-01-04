819531ab6a014bb9ef5016936cf6a6ee7ca0c1f6
benyamynbrkyc <benjamin.brkic.a@gmail.com>
2022-01-05 00:19:39 +0100

    get only published books

--- api/books/controllers/books.js
+++ api/books/controllers/books.js
@@ -1,5 +1,6 @@
 "use strict";
 const { sanitizeEntity } = require("strapi-utils");
+const qs = require("querystring");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -11,32 +12,34 @@ module.exports = {
     const entities = await strapi.query("books").find({});
 
     // TODO: delete created_by ... from cover object
-    const books = entities.map((book) => {
-      delete book.e_book_epub;
-      if (book.author) {
-        delete book.author.user;
-        delete book.author.created_by;
-        delete book.author.updated_by;
-        delete book.author.created_at;
-        delete book.author.updated_at;
-      }
-      if (book.publisher) {
-        delete book.publisher.created_by;
-        delete book.publisher.updated_by;
-        delete book.publisher.created_at;
-        delete book.publisher.updated_at;
-        delete book.publisher.published_at;
-      }
-      delete book.published_at;
-      delete book.created_by;
-      delete book.updated_by;
-      delete book.created_at;
-      delete book.updated_at;
-      delete book.cover.name;
-      delete book.e_book_pdf;
+    const books = entities
+      .filter((book) => book.published_at != null)
+      .map((book) => {
+        delete book.e_book_epub;
+        if (book.author) {
+          delete book.author.user;
+          delete book.author.created_by;
+          delete book.author.updated_by;
+          delete book.author.created_at;
+          delete book.author.updated_at;
+        }
+        if (book.publisher) {
+          delete book.publisher.created_by;
+          delete book.publisher.updated_by;
+          delete book.publisher.created_at;
+          delete book.publisher.updated_at;
+          delete book.publisher.published_at;
+        }
+        // delete book.published_at;
+        delete book.created_by;
+        delete book.updated_by;
+        delete book.created_at;
+        delete book.updated_at;
+        delete book.cover.name;
+        delete book.e_book_pdf;
 
-      return book;
-    });
+        return book;
+      });
 
     return books;
   },
