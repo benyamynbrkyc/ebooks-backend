c546eddb66fee1cfafb42ed48faa28f5d9b1fb83
benyamynbrkyc <benjamin.brkic.a@gmail.com>
2022-01-05 01:59:43 +0100

    create route to enumerate publishers

--- api/publishers/controllers/publishers.js
+++ api/publishers/controllers/publishers.js
@@ -1,8 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async enumPublishers(ctx) {
+    try {
+      const entities = await strapi
+        .query("publishers")
+        .find({}, ["id", "name"]);
+
+      entities.forEach((e) => {
+        delete e.created_by;
+        delete e.created_at;
+        delete e.updated_by;
+        delete e.updated_at;
+      });
+
+      ctx.send(entities);
+    } catch (error) {
+      console.error(error);
+      ctx.throw(500);
+    }
+  },
+};
