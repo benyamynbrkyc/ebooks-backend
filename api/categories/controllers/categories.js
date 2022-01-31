6801f0d2cda349f73f5b0de5f0775fb2a3c92e69
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-01-31 01:59:10 +0100

    feat(categories controller): sanitize categories entities
    Remove metadata from response

--- api/categories/controllers/categories.js
+++ api/categories/controllers/categories.js
@@ -1,8 +1,32 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.categories.search(ctx.query);
+    } else {
+      entities = await strapi.services.categories.find(ctx.query);
+    }
+
+    return entities.map((entity) => {
+      delete entity.published_at;
+      delete entity.created_at;
+      delete entity.updated_at;
+      return sanitizeEntity(entity, { model: strapi.models.categories });
+    });
+  },
+};
