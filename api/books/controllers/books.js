6768a3d6da264de0f74d02f4a241a8fb83fead6a
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-02-08 02:05:16 +0100

    feat(books): enable bestsellers route

--- api/books/controllers/books.js
+++ api/books/controllers/books.js
@@ -42,6 +42,43 @@ module.exports = {
 
     return books;
   },
+  async findBestsellers() {
+    const entities = await strapi
+      .query("books")
+      .find({ _limit: 8, _sort: "total_sold:DESC" });
+
+    // TODO: delete created_by ... from cover object
+    const books = entities
+      .filter((entity) => entity.published_at != null)
+      .map((book) => {
+        delete book.e_book_epub;
+        if (book.author) {
+          delete book.author.user;
+          delete book.author.created_by;
+          delete book.author.updated_by;
+          delete book.author.created_at;
+          delete book.author.updated_at;
+        }
+        if (book.publisher) {
+          delete book.publisher.created_by;
+          delete book.publisher.updated_by;
+          delete book.publisher.created_at;
+          delete book.publisher.updated_at;
+          delete book.publisher.published_at;
+        }
+        // delete book.published_at;
+        delete book.created_by;
+        delete book.updated_by;
+        delete book.created_at;
+        delete book.updated_at;
+        delete book.cover.name;
+        delete book.e_book_pdf;
+
+        return book;
+      });
+
+    return books;
+  },
   async findOnePublic(ctx) {
     const { id } = ctx.params;
 
