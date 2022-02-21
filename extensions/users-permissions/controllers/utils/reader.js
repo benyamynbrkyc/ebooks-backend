3702fc902d55abc0bbc23aea8f8e3c195bae0a13
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-02-22 00:01:29 +0100

    refactor(reader): rename function

--- extensions/users-permissions/controllers/utils/reader.js
+++ extensions/users-permissions/controllers/utils/reader.js
@@ -1,11 +1,11 @@
 const { sanitizeEntity } = require("strapi-utils");
 
-const getBook = async (id) => {
+const getBookReaderUrl = async (id) => {
   try {
     const entity = await strapi.services.books.findOne({ id });
     const book = sanitizeEntity(entity, { model: strapi.models.books });
 
-    const eBookUrl = process.env.BASE_API_URL + book.e_book_epub.url;
+    const eBookUrl = process.env.BASE_API_URL + book.e_book_pdf.url;
     return eBookUrl;
   } catch (error) {
     console.error(error);
@@ -15,4 +15,4 @@ const getBook = async (id) => {
   }
 };
 
-module.exports = { getBook };
+module.exports = { getBookReaderUrl };
