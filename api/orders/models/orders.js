a782532863e1b3857150003a1f56ad4fc0d913e3
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-02-25 03:01:36 +0100

    refactor(file): move user fetching to top

--- api/orders/models/orders.js
+++ api/orders/models/orders.js
@@ -5,19 +5,19 @@ module.exports = {
     async afterCreate(result, data) {
       if (!data.user || data.order_type == "print") return;
 
+      const userId = data.user.id;
+      const user = await strapi.plugins[
+        "users-permissions"
+      ].services.user.fetch({
+        id: userId,
+      });
+
       try {
         const orderId = result.id;
-        const userId = data.user.id;
         await strapi.services.orders.update(
           { id: orderId },
           { user: { id: userId } }
         );
-
-        const user = await strapi.plugins[
-          "users-permissions"
-        ].services.user.fetch({
-          id: userId,
-        });
 
         const userAlreadyOwnedBooksIds = user.owned_books.map(
           (book) => book.id
