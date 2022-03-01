a61d4f96fcf95df7975afa51a7ad0e932d0a58af
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-03-01 23:07:59 +0100

    refactor(server config): use PUBLIC_URL env var

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("URL", "localhost"),
+  url: env("PUBLIC_URL", "http://localhost:1337"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "96fae9f0070ad3de2498cc6e146c119a"),
