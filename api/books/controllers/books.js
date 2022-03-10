f30b42ca666f11ef30f6a025b16042905a3ced69
benyamynbrkyc <benjamin.brkic.a@gmail.com>
2022-03-10 14:13:23 +0000

    remove unnecessary code

--- api/books/controllers/books.js
+++ api/books/controllers/books.js
@@ -10,31 +10,11 @@ module.exports = {
   async findPublic() {
     const entities = await strapi.query("books").find({ _limit: -1 });
 
-    // TODO: delete created_by ... from cover object
     const books = entities
       .filter((entity) => entity.published_at != null)
       .map((book) => {
-        book?.e_book_epub && delete book.e_book_epub;
-        if (book.author) {
-          delete book.author.user;
-          delete book.author.created_by;
-          delete book.author.updated_by;
-          delete book.author.created_at;
-          delete book.author.updated_at;
-        }
-        if (book.publisher) {
-          delete book.publisher.created_by;
-          delete book.publisher.updated_by;
-          delete book.publisher.created_at;
-          delete book.publisher.updated_at;
-          delete book.publisher.published_at;
-        }
-        // delete book.published_at;
-        book?.created_by && delete book.created_by;
-        book?.updated_by && delete book.updated_by;
-        book?.created_at && delete book.created_at;
-        book?.updated_at && delete book.updated_at;
-        book?.e_book_pdf && delete book.e_book_pdf;
+        if (book.e_book_epub) delete book.e_book_epub;
+        if (book.e_book_pdf) delete book.e_book_pdf;
 
         return book;
       });
@@ -46,31 +26,11 @@ module.exports = {
       .query("books")
       .find({ _limit: 8, _sort: "total_sold:DESC" });
 
-    // TODO: delete created_by ... from cover object
     const books = entities
       .filter((entity) => entity.published_at != null)
       .map((book) => {
-        book?.e_book_epub && delete book.e_book_epub;
-        if (book.author) {
-          delete book.author.user;
-          delete book.author.created_by;
-          delete book.author.updated_by;
-          delete book.author.created_at;
-          delete book.author.updated_at;
-        }
-        if (book.publisher) {
-          delete book.publisher.created_by;
-          delete book.publisher.updated_by;
-          delete book.publisher.created_at;
-          delete book.publisher.updated_at;
-          delete book.publisher.published_at;
-        }
-
-        book?.created_by && delete book.created_by;
-        book?.updated_by && delete book.updated_by;
-        book?.created_at && delete book.created_at;
-        book?.updated_at && delete book.updated_at;
-        book?.e_book_pdf && delete book.e_book_pdf;
+        if (book.e_book_epub) delete book.e_book_epub;
+        if (book.e_book_pdf) delete book.e_book_pdf;
 
         return book;
       });
