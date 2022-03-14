933a32e92fc3ef7516bc80b2d70e6f01c96fdd42
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-03-14 16:29:38 +0100

    refactor(order controller): remove create controller

--- api/orders/controllers/orders.js
+++ api/orders/controllers/orders.js
@@ -24,18 +24,18 @@ module.exports = {
    * @return {Object}
    */
 
-  async create(ctx) {
-    let entity;
+  // async create(ctx) {
+  //   let entity;
 
-    if (ctx.is("multipart")) {
-      const { data, files } = parseMultipartData(ctx);
-      entity = await strapi.services.restaurant.create(data, { files });
-    } else {
-      entity = await strapi.services.restaurant.create(ctx.request.body);
-    }
+  //   if (ctx.is("multipart")) {
+  //     const { data, files } = parseMultipartData(ctx);
+  //     entity = await strapi.services.restaurant.create(data, { files });
+  //   } else {
+  //     entity = await strapi.services.restaurant.create(ctx.request.body);
+  //   }
 
-    return sanitizeEntity(entity, { model: strapi.models.restaurant });
-  },
+  //   return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  // },
   async verifyOrder(ctx) {
     const orderId = ctx.request.body.orderId;
 
