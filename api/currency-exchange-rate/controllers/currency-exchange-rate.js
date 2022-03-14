2e5a9979ce59b4801ef53d76263442cf729cb8ef
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-03-14 22:01:26 +0100

    feat(XR): add custom controller

--- api/currency-exchange-rate/controllers/currency-exchange-rate.js
+++ api/currency-exchange-rate/controllers/currency-exchange-rate.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let XR = "1.95";
+    try {
+      const xrData = await strapi
+        .query("currency-exchange-rate")
+        .findOne({ id: 1 });
+      XR = xrData.eur_to_bam_rate;
+    } catch (error) {
+      console.log("An error occurred while fetching exchange rate data.");
+    } finally {
+      ctx.send(XR);
+    }
+  },
+};
