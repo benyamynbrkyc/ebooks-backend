d2da832e19c3f0a2c4b9fa557a2f756ea32dd5df
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-06-21 23:39:40 +0200

    fix xr

--- api/currency-exchange-rate/controllers/currency-exchange-rate.js
+++ api/currency-exchange-rate/controllers/currency-exchange-rate.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const axios = require("axios");
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -7,16 +9,7 @@
 
 module.exports = {
   async find(ctx) {
-    let XR = "1.95";
-    try {
-      const xrData = await strapi
-        .query("currency-exchange-rate")
-        .findOne({ id: 1 });
-      XR = xrData.eur_to_bam_rate;
-    } catch (error) {
-      console.log("An error occurred while fetching exchange rate data.");
-    } finally {
-      ctx.send(XR);
-    }
+    const XR = await strapi.services["currency-exchange-rate"].fetchXR();
+    ctx.send(XR);
   },
 };
