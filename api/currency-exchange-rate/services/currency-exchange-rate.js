de6ec50880a46d0f49d05c133f37fad9567a2a83
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-07-06 20:53:51 +0200

    use constant xr

--- api/currency-exchange-rate/services/currency-exchange-rate.js
+++ api/currency-exchange-rate/services/currency-exchange-rate.js
@@ -5,46 +5,8 @@
  * to customize this service
  */
 
-const axios = require("axios");
-
-// this is such ugly code, but it works
 module.exports = {
   async fetchXR() {
-    const getXR = async () => {
-      const { data } = await axios.get(process.env.EXCHANGE_RATE_API_ENDPOINT);
-      const bamXR = data.rates.BAM.toFixed(2);
-
-      return bamXR;
-    };
-
-    let XR = "1.95";
-    try {
-      const xrData = await strapi
-        .query("currency-exchange-rate")
-        .findOne({ id: 1 });
-
-      const xrOlderThanOneDay =
-        xrData?.updated_at < getOlderThanCurrentDayTimestampValue();
-
-      if (!xrData || xrOlderThanOneDay) {
-        XR = await getXR();
-
-        await strapi
-          .query("currency-exchange-rate")
-          .update({ id: 1 }, { eur_to_bam_rate: parseFloat(XR) });
-
-        console.log("Updated xr");
-      } else {
-        XR = xrData.eur_to_bam_rate;
-      }
-    } catch (error) {
-      console.error(error);
-      console.log("An error occurred while fetching exchange rate data.");
-    } finally {
-      return XR;
-    }
+    return "1.95583";
   },
 };
-
-const getOlderThanCurrentDayTimestampValue = () =>
-  new Date(Date.now() - 1000 * 60 * 60 * 24);
